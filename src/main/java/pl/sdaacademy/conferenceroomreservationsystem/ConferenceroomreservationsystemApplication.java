4f441b2fa2db2fdbfbe2c855e71d6af09f98f75c
Rafał Borowczyk <Rafal.Borowczyk@protonmail.com>
2023-03-04 12:49:55 +0100

    first commit

---
+++ src/main/java/pl/sdaacademy/conferenceroomreservationsystem/ConferenceroomreservationsystemApplication.java
@@ -0,0 +1,13 @@
+package pl.sdaacademy.conferenceroomreservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceroomreservationsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceroomreservationsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sdaacademy/conferenceroomreservationsystem/ConferenceroomreservationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdaacademy.conferenceroomreservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceroomreservationsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
