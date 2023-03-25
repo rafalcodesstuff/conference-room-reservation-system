7499d90b5e20769cf6d85f48d6bb5dfc28bc7b33
Rafał Borowczyk <Rafal.Borowczyk@protonmail.com>
2023-03-25 12:11:14 +0100

    Change(CRRS): re-did the entire project, added frontend, made controllers/serviec/repos generic, added security with session

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReservationsAddFormComponent } from './reservations/reservations-add-form/reservations-add-form.component';
+import { ReservationsEditorComponent } from './reservations/reservations-editor/reservations-editor.component';
+import { ReservationsMainComponent } from './reservations/reservations-main/reservations-main.component';
+import { LoginComponent } from './startpage/login/login.component';
+import { RegisterComponent } from './startpage/register/register.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "calendar", component: ReservationsMainComponent },
+  { path: "calendar/add", component: ReservationsAddFormComponent },
+  { path: "calendar/edit", component: ReservationsEditorComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ReservationsModule } from "./reservations/reservations.module";
+import { StartpageModule } from './startpage/startpage.module';
+import { CalendarService } from './reservations/calendar-service.service';
+import { UserService } from './user-service.service';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    providers: [CalendarService, UserService],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        CommonModule,
+        NgbModule,
+        RouterModule,
+        HttpClientModule,
+        ReservationsModule,
+        StartpageModule
+    ]
 })
 export class AppModule { }
